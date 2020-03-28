71eabe2616b275e606c7ee2a357e9b4c2f59854f
Egor Molkov <molkov@i-teco.ru>
2020-03-29 02:17:00 +0300

    + 17 homeWork

--- src/test/java/com/github/m0levich/SeleniumHW.java
+++ src/test/java/com/github/m0levich/SeleniumHW.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,6 +32,7 @@ public class SeleniumHW {
 
     @Test
     public void seleniumHW_2() {
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.get("https://savkk.github.io/selenium-practice/");
         webDriver.findElement(By.id("button")).click();
         webDriver.findElement(By.id("first")).click();
@@ -37,9 +41,7 @@ public class SeleniumHW {
         WebElement button = webDriver.findElement(By.xpath("//input[1]"));
         assertThat(button.getAttribute("value")).isEqualToIgnoringCase("CLICK ME TOO!");
         button.click();
-        WebElement returnButton = webDriver.findElement(By.xpath("//label[@id='back']/a"));
-        Assert.assertEquals(returnButton.getText(), "Great! Return to menu");
-        returnButton.click();
+        returnToMenu();
         webDriver.findElement(By.id("checkbox")).click();
         WebElement checkOne = webDriver.findElement(By.id("one"));
         checkOne.click();
@@ -47,18 +49,64 @@ public class SeleniumHW {
         checkTwo.click();
         webDriver.findElement(By.id("go")).click();
         WebElement resultCheckboxes = webDriver.findElement(By.id("result"));
-        Assert.assertEquals(resultCheckboxes.getText(),checkOne.getAttribute("value") + " " + checkTwo.getAttribute("value"));
+        Assert.assertEquals(resultCheckboxes.getText(), checkOne.getAttribute("value") + " " + checkTwo.getAttribute("value"));
         WebElement radioTwo = webDriver.findElement(By.id("radio_two"));
         radioTwo.click();
         webDriver.findElement(By.id("radio_go")).click();
         WebElement radioResult = webDriver.findElement(By.id("radio_result"));
-        Assert.assertEquals(radioResult.getText(),radioTwo.getAttribute("value"));
-        WebElement returnMenu = webDriver.findElement(By.xpath("//label[@id='back']/a"));
-        Assert.assertEquals(returnMenu.getText(),"Great! Return to menu");
+        Assert.assertEquals(radioResult.getText(), radioTwo.getAttribute("value"));
+        returnToMenu();
+        webDriver.findElement(By.xpath("//a[.='Select']")).click();
+        WebElement selectHero = webDriver.findElement(By.name("hero"));
+        Select select = new Select(selectHero);
+        select.selectByVisibleText("Niklaus Wirth");
+        WebElement selectLanguages = webDriver.findElement(By.name("languages"));
+        Select selectLang = new Select(selectLanguages);
+        selectLang.selectByVisibleText("Java");
+        selectLang.selectByVisibleText("Basic");
+        webDriver.findElement(By.id("go")).click();
+        WebElement selectResultHero = webDriver.findElement(By.xpath("//label[@name='result'][1]"));
+        Assert.assertEquals("Niklaus Wirth", selectResultHero.getText());
+        WebElement selectResultLang = webDriver.findElement(By.xpath("//label[@name='result'][2]"));
+        Assert.assertEquals("Java, Basic", selectResultLang.getText());
+        returnToMenu();
+        webDriver.findElement(By.id("form")).click();
+        fillFild("First Name:", "Ivan");
+        fillFild("Last Name:", "Ivanov");
+        fillFild("Email:", "ivantest@mail.ru");
+        webDriver.findElement(By.xpath("//label[.='Sex:']/following-sibling::input[1]")).click();
+        fillFild("Address:", "12, Lenina street,Moscow");
+        webDriver.findElement(By.xpath("//textarea")).sendKeys("normal test information");
+        webDriver.findElement(By.xpath("//label[.='Avatar:']/following-sibling::input[1]")).sendKeys(System.getProperty("user.dir") + "/picture.jpeg");
+        webDriver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
+        returnToMenu();
+        webDriver.findElement(By.id("iframe")).click();
+        webDriver.switchTo().frame(0);
+        WebElement codeElement = webDriver.findElement(By.id("code"));
+        String code = codeElement.getText();
+
+        for (String retval : code.split(" ")) {
+            code = retval;
+        }
+
+        webDriver.switchTo().defaultContent();
+        webDriver.findElement(By.xpath("//input[@name='code']")).sendKeys(code);
+        webDriver.findElement(By.xpath("//input[@name='ok']")).click();
+        returnToMenu();
     }
 
     @AfterMethod
     public void closeDriver() {
         webDriver.quit();
+    }
+
+    public void returnToMenu() {
+        WebElement returnMenu = webDriver.findElement(By.xpath("//label[@id='back']/a"));
+        Assert.assertEquals(returnMenu.getText(), "Great! Return to menu");
+        returnMenu.click();
+    }
+
+    public void fillFild(String fieldTitle, String value) {
+        webDriver.findElement(By.xpath("//label[.='" + fieldTitle + "']/following-sibling::input[1]")).sendKeys(value);
     }
 }
